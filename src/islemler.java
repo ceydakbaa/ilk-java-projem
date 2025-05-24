8432cada99c3240a44d729fe23dcce28fe553d19
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-24 22:16:28 +0300

    ilk commit

---
+++ src/islemler.java
@@ -0,0 +1,18 @@
+public class islemler {
+    public static void main(String[] args){
+
+        double number1=7;
+        double number2 =5;
+
+        double cikarma= number1-number2;
+        System.out.println("çıkarma"+cikarma);
+        double toplama= number1+number2;
+        System.out.println("toplama"+ toplama);
+        double carpma= number1*number2;
+        System.out.println("çarpma"+carpma);
+        double bolme= number1/number2;
+        System.out.println("bölme"+bolme);
+
+
+    }
+}
