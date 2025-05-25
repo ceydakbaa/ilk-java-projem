7b735f994b0ee040c42298758ef24cd2a0060219
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-25 12:35:45 +0300

    ilk commit

---
+++ src/fordöngüsü.java
@@ -0,0 +1,10 @@
+public class fordöngüsü {
+    public static void main(String[] args){
+        /*
+        * for(baslangıç;durum;arttırma){//kodla}
+         */
+        for(int i =1; i<=10;  i++){
+            System.out.println(i);
+        }
+    }
+}
