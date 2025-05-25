5f6926382a39581a19a16826d4f5fad8c9d603f4
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-25 16:11:28 +0300

    ilk commit

---
+++ src/elemanlıdöngü.java
@@ -0,0 +1,9 @@
+public class elemanlıdöngü {
+    public static void main(String[] args){
+        int [] numbers= {3,5,7,11,13};
+        for (int i =0; i<5; i++){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
