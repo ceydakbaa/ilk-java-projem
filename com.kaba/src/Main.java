60f771c9be0c3928f92c9be8dd0db2a0c448c983
Ceyda Kaba <ceydakaba@gmail.com>
2025-05-31 13:27:03 +0300

    ilk commit

--- com.kaba/src/Main.java
+++ com.kaba/src/Main.java
@@ -1,11 +1,13 @@
+import java.util.concurrent.Callable;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int sonuc= carp(12,8);
-        System.out.println("Sonuç:"+ sonuc);
-    }
-    public static int carp(int a, int b){
-        return a*b;
+       Vehicle vehicle =new Vehicle();
+       vehicle.start();
+
+        Car car = new Car();
+        car.start();
     }
 }
