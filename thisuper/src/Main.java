58a822f9f4a47d6fed705f78571a11f04e6874ea
Ceyda Kaba <ceydakaba@gmail.com>
2025-06-03 11:47:10 +0300

    ilk commit

--- thisuper/src/Main.java
+++ thisuper/src/Main.java
@@ -1,9 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+   // public static void main(String[] args) {
+ //       Person person = new Person("Sude",8,"girl");
+   //     Student student = new Student("Sude",8,"girl",100);
+       // System.out.println(student.getName());
+
+// Enum projesi main kısmı
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Sude",8,"girl");
-        Student student = new Student("Sude",8,"girl",100);
-        System.out.println(student.getName());
+        Days bugün = Days.Friday;
+        System.out.println("Today is " + bugün);
     }
 }
